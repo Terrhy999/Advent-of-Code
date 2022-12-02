9e0d7e8bdc61c48cb80737e360a1ccedcb19608e
terrhy999 <thyla999@gmail.com>
2022-12-02 13:37:23 -0500

    changed folder structure, each days problems share a folder now

--- 2021/day01/src/main.rs
+++ 2021/day01/src/main.rs
@@ -3,6 +3,26 @@ use std::io::{BufRead, BufReader};
 
 fn main() {
     let input = File::open("input.txt").expect("Cannot open .txt file");
+    part1(&input);
+    part2(&input);
+}
+
+fn part1(input: &File) {
+    let reader = BufReader::new(input);
+
+    let count = reader
+        .lines()
+        .map(|x| x.unwrap().parse().unwrap())
+        .collect::<Vec<u16>>()
+        .windows(2)
+        .filter(|x| x[0] < x[1])
+        .count();
+
+    println!("{}", count);
+}
+
+fn part2(input: &File) {
+    let input = File::open("input.txt").expect("Cannot open .txt file");
     let reader = BufReader::new(input);
 
     let count = reader
--- 2022/day01/src/main.rs
+++ 2022/day01/src/main.rs
@@ -2,6 +2,20 @@ use std::fs;
 
 fn main() {
     let input: String = fs::read_to_string("./input.txt").expect("Couldn't read the file");
+    part1(&input);
+    part2(&input);
+}
+
+fn part1(input: &String) {
+    let elfs_total_calories = input
+        .split("\n\n")
+        .map(|elf| elf.lines().map(|meal| meal.parse::<i32>().unwrap()).sum())
+        .collect::<Vec<i32>>();
+
+    println!("{}", elfs_total_calories.iter().max().unwrap())
+}
+
+fn part2(input: &String) {
     let mut elfs_total_calories = input
         .split("\n\n")
         .map(|elf| elf.lines().map(|meal| meal.parse::<i32>().unwrap()).sum())
