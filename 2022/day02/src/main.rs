77316e2249826aed72f4ce376b54c1373fec4ae2
terrhy999 <thyla999@gmail.com>
2022-12-02 13:47:22 -0500

    2022 day02b solved

--- 2022/day02/src/main.rs
+++ 2022/day02/src/main.rs
@@ -2,7 +2,11 @@ use std::fs;
 
 fn main() {
     let input = fs::read_to_string("./input.txt").unwrap();
+    println!("Part 1: {}", part1(&input));
+    println!("Part 2: {}", part2(&input));
+}
 
+fn part1(input: &String) -> i32 {
     let rounds = input.lines().collect::<Vec<&str>>();
 
     let mut total = 0;
@@ -24,5 +28,29 @@ fn main() {
         total += result;
     }
 
-    println!("{}", total);
+    total
+}
+
+fn part2(input: &String) -> i32 {
+    let rounds = input.lines().collect::<Vec<&str>>();
+
+    let mut total = 0;
+
+    for round in rounds {
+        let result = match round {
+            "B X" => 1,
+            "C X" => 2,
+            "A X" => 3,
+            "A Y" => 4,
+            "B Y" => 5,
+            "C Y" => 6,
+            "C Z" => 7,
+            "A Z" => 8,
+            "B Z" => 9,
+            _ => 0,
+        };
+
+        total += result;
+    }
+    total
 }
