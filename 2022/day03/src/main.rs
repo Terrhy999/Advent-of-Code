853cdc23437593d32905b76b87ad3236a33b6e4b
terrhy999 <thyla999@gmail.com>
2022-12-05 10:18:27 -0500

    2022 day03 part1 solved

--- 2022/day03/src/main.rs
+++ 2022/day03/src/main.rs
@@ -1,23 +1,82 @@
-use array_tool::vec::*;
-use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::fs;
 
 fn main() {
-    let file = File::open("./input.txt").expect("couldn't read the file");
-    let input = BufReader::new(file);
+    let input = fs::read_to_string("./input.txt").expect("couldn't read the file");
+    let lines: Vec<&str> = input.lines().collect();
+    println!("{}", part1(lines));
 }
 
-fn part1(input: BufReader<File>) {
-    let lines = input.lines();
-    let backpacks = lines.map(|line| line.unwrap());
+fn part1(input: Vec<&str>) -> i32 {
+    let backpacks: Vec<i32> = input
+        .iter()
+        .map(|backpack| {
+            let (container1, container2) = backpack.split_at(backpack.len() / 2);
+            let mut common_char = ' ';
+            for item in container1.chars() {
+                if container2.contains(item) {
+                    common_char = item;
+                }
+            }
+            item_to_score(common_char)
+        })
+        .collect();
+    backpacks.iter().sum()
+}
 
-    let compartments = backpacks.map(|backpack| {
-        let compartment_size = backpack.len() / 2;
-        let (compartment1, compartment2) = backpack.split_at(compartment_size);
-        [compartment1, compartment2]
-    });
-
-    let compartments_intersection = compartments.map(|compartment| {
-        let item_in_common = 
-    })
+fn item_to_score(item: char) -> i32 {
+    match item {
+        'a' => 1,
+        'b' => 2,
+        'c' => 3,
+        'd' => 4,
+        'e' => 5,
+        'f' => 6,
+        'g' => 7,
+        'h' => 8,
+        'i' => 9,
+        'j' => 10,
+        'k' => 11,
+        'l' => 12,
+        'm' => 13,
+        'n' => 14,
+        'o' => 15,
+        'p' => 16,
+        'q' => 17,
+        'r' => 18,
+        's' => 19,
+        't' => 20,
+        'u' => 21,
+        'v' => 22,
+        'w' => 23,
+        'x' => 24,
+        'y' => 25,
+        'z' => 26,
+        'A' => 27,
+        'B' => 28,
+        'C' => 29,
+        'D' => 30,
+        'E' => 31,
+        'F' => 32,
+        'G' => 33,
+        'H' => 34,
+        'I' => 35,
+        'J' => 36,
+        'K' => 37,
+        'L' => 38,
+        'M' => 39,
+        'N' => 40,
+        'O' => 41,
+        'P' => 42,
+        'Q' => 43,
+        'R' => 44,
+        'S' => 45,
+        'T' => 46,
+        'U' => 47,
+        'V' => 48,
+        'W' => 49,
+        'X' => 50,
+        'Y' => 51,
+        'Z' => 52,
+        _ => 0,
+    }
 }
